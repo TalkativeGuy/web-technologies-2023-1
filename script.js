1ff343714e01575d3f03d5993b9d0e68ff9b3f41
TalkativeGuy <vLaZoV_007@mail.ru>
2024-01-09 06:49:41 +0500

    done

--- script.js
+++ script.js
@@ -1,124 +1,92 @@
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', init)
-} else {
-    init()
-}
+Vue.component("directory-item", {
+	data: function() {
+		return {
+			childrenShown: false
+		}
+	},
+	props: [ 'data' ],
+	template: `
+		<div class="list-item" :class="childrenShown ? 'list-item_open' : ''">
+			<div class="list-item__inner" @click="childrenShown = !childrenShown && data.hasChildren">
+				<img class="list-item__arrow" :class="!data.hasChildren ? 'list-item__arrow_invisible' : ''" src="img/chevron-down.png" alt="chevron-down">
+				<img class="list-item__folder" src="img/folder.png" alt="folder">
+				<span>{{ data.name }}</span>
+			</div>
+			<div class="list-item__items" v-show="childrenShown">
+				<directory-item v-for="item in data.items" :data="item"></directory-item>
+			</div>
+		</div>
+	`
+})
 
-function init() {
-    const data = {
-        name: 'Каталог товаров',
-        hasChildren: true,
-        items: [
-            {
-                name: 'Мойки',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran1',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT1',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT2',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: 'Ulgran2',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT3',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT4',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    }
-                ]
-            },{
-                name: 'Фильтры',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran3',
-                        hasChildren: true,
-                        items: [
-                            {
-                                name: 'SMT5',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'SMT6',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
-
-
-    const items = new ListItems(document.getElementById('list-items'), data)
-
-
-  /*  items.render()*/
-    items.init()
-
-    /*console.log(items.renderTest(data));*/
-
-    function ListItems(el, data) {
-        this.el = el;
-        this.data = data;
-
-        this.init = function () {
-            const parents = this.el.querySelectorAll('[data-parent]')
-
-            parents.forEach(parent => {
-                const open = parent.querySelector('[data-open]')
-
-                open.addEventListener('click', () => this.toggleItems(parent) )
-            })
-        }
-
-        this.render = function () {
-            this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
-        }
-
-        this.renderParent = function (data) {
-            //проверка всех элементов на hasChildren
-            //если hasChildren, то запускаем renderParent
-            //если !hasChildren, то запускаем renderChildren
-            //возвращает рендер родительского элемента
-
-        }
-
-        this.renderChildren = function (data) {
-            //вовзращает рендер элемента без вложенности
-        }
-
-        this.toggleItems = function (parent) {
-            parent.classList.toggle('list-item_open')
-        }
-
-/*        this.renderTest = function (data) {
-            return `
-            <div class="test">${data.name}</div>
-            `
-        }*/
-    }
-
-}
+var app = new Vue({
+	el: '#app',
+	data: {
+	  	data: {
+			name: 'Каталог товаров',
+			hasChildren: true,
+			items: [
+				{
+					name: 'Мойки',
+					hasChildren: true,
+					items: [
+						{
+							name: 'Ulgran1',
+							hasChildren: true,
+							items: [
+								{
+									name: 'SMT1',
+									hasChildren: false,
+									items: []
+								},
+								{
+									name: 'SMT2',
+									hasChildren: false,
+									items: []
+								}
+							]
+						},
+						{
+							name: 'Ulgran2',
+							hasChildren: true,
+							items: [
+								{
+									name: 'SMT3',
+									hasChildren: false,
+									items: []
+								},
+								{
+									name: 'SMT4',
+									hasChildren: false,
+									items: []
+								}
+							]
+						}
+					]
+				},
+				{
+					name: 'Фильтры',
+					hasChildren: true,
+					items: [
+						{
+							name: 'Ulgran3',
+							hasChildren: true,
+							items: [
+								{
+									name: 'SMT5',
+									hasChildren: false,
+									items: []
+								},
+								{
+									name: 'SMT6',
+									hasChildren: false,
+									items: []
+								}
+							]
+						}
+					]
+				}
+			]
+		}
+	}
+})
