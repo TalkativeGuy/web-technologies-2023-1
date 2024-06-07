cbb90650faa55ae6ab212bd64c0bc021127c4c4d
TalkativeGuy <vLaZoV_007@mail.ru>
2024-06-07 17:14:48 +0500

    vite+vue project init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue 3 + TypeScript + Vite
+
+Запуск:
+
+```
+npm run dev
+```
+
+Затем перейти по ссылке
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+	
+</div>
+</template>
+	
+	
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	components: { },
+	data() {
+		return {
+		}
+	},
+	computed: {
+	},
+	mounted() {
+	},
+	methods: {
+	},
+})
+</script>
+	
+	
+<style lang="scss" scoped>
+	
+</style>
